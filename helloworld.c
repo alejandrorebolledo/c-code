391bf55018d1ceeb5bded83b8312cc49bd1701c3
alejandrorebolledo <54950946+alejandrorebolledo@users.noreply.github.com>
2021-04-01 10:43:14 -0700

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c
+// compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+
+int main(){
+   printf ("HelloWorld\n");
+   printf("Hola Mundo\n");
+   printf("AAAAAA\n");
+   return 0;
+}   
