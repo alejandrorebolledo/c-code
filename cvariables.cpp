861061e5acb1ee450a9b8a6a1f66767a68d22fd5
alejandrorebolledo <54950946+alejandrorebolledo@users.noreply.github.com>
2021-04-01 10:21:20 -0700

    Add files via upload

---
+++ cvariables.cpp
@@ -0,0 +1,31 @@
+// cvariables.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+	// Initialize variables
+	int i ;  //-integers
+	double d; //-floting point numbers
+	float f; // - floating point number that a smaller than double
+	char c ;  //-stores text, "Hello World".
+	bool  done ; //  true or false
+	// assign data to each variable
+	i = 65521;
+	d = 3.1415926535897932384626;
+	f = 2.7182818284590;
+	c = 'A';
+	s = "This is a string of characters.";
+	done = true;
+	// print the variables data
+	cout << i << "\n";
+	cout << d << "\n";
+	cout << f << "\n";
+	cout << c << "\n";
+	cout << s << "\n";
+	cout << done << "\n";
+	done = false;
+	cout << done<< "\n";
+	return 0;
+}
+	
+	
